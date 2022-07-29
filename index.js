c5a00138849c72fd4894d67090c0c003fcfc6efb
Michael Metcalf <mikegmac14@gmail.com>
2022-07-29 16:49:55 -0700

    Add funstion to generate a function to pick Rock, Paper, or Scissors for computer

---
+++ index.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let randomChoice = Math.floor((Math.random() * 3) + 1);
+    if (randomChoice === 1) {
+        return "Rock";
+    } else if (randomChoice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
