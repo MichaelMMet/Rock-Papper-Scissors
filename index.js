94cc6417acf81de78d1517539709e494b0238eb0
Michael Metcalf <mikegmac14@gmail.com>
2022-08-07 19:11:47 -0700

    Add buttons to play with and simple UI that shows score, result of the  round, and result of the game after 5 rounds.

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ let computerScore = 0;
 
 let playerScore = 0;
 
+let rounds = 0;
+
+const rock = document.querySelector(".rock");
+
+const paper = document.querySelector(".paper");
+
+const scissors = document.querySelector(".scissors");
+
+const results = document.querySelector(".results");
+const score = document.querySelector(".score");
+
+
+
+rock.addEventListener('click', playGame);
+paper.addEventListener("click", playGame);
+scissors.addEventListener("click", playGame);
+
+
+
+
+
+
 //computer picks between rock, paper, or sciccors at random
 function getComputerChoice() {
     let randomChoice = Math.floor((Math.random() * 3) + 1);
@@ -16,113 +38,75 @@ function getComputerChoice() {
 }
 
 //runs a prompt to get a choice from the user
-function getPlayerChoice() {
-    let playerChoice = prompt("Enter Rock, Paper, or Scissors");
-    return playerChoice;
+function getPlayerChoice(playerChoice) {
+    if (playerChoice === "rock") {
+        return "rock";
+    } else if (playerChoice === "paper") {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 
-
 //plays one round
-function playRound() {
-    let playerChoice = getPlayerChoice();
+function playRound(choice) {
+    let playerChoice = getPlayerChoice(choice);
     let cpuChoice = getComputerChoice();
     cpuChoice = cpuChoice.toLowerCase();
-    playerChoice = playerChoice.toLowerCase();
     console.log(cpuChoice);
+    console.log(playerChoice)
 
 
     //checks for a tie then to see if the Computer won the round
     if (playerChoice === cpuChoice) {
-        alert("Its a tie! Next Round!");
+        rounds++;
+        score.textContent = playerScore + " - " + computerScore;
+        results.textContent = "Its a tie! Next Round!";
     } else if ((cpuChoice === "rock" && playerChoice === "scissors") ||
         (cpuChoice === "paper" && playerChoice === "rock") ||
         (cpuChoice === "scissors" && playerChoice === "paper")) {
-        alert("Cumputer wins this round!");
+        rounds++;
         computerScore++;
+        score.textContent = playerScore + " - " + computerScore;
+        results.textContent = "Cumputer wins this round!";
+
     } else {
-        alert("You win this round!");
+        rounds++;
         playerScore++;
-    }
-
-    //two cases of me trying to use the equalsIgnoreCase to no avail, kept for future reference
-    
-    /*
-        if(playerChoice.equalsIgnoreCase("Rock") && cpuChoice === "Scissors"){
-          console.log("Rock beats Scissors, you win!");
-        } else if(playerChoice.equalsIgnoreCase("Rock") && cpuChoice === "Rock"){
-            console.log("Its a tie! Try again!");
-        } else if(playerChoice.equalsIgnoreCase("Rock") && cpuChoice === "Paper"){
-    console.log("Paper beats Rock, computer wins!");
-        }
-    */
-
-    /*
-    switch (playerChoice) {
-
-        case playerChoice === "rock":
-            if (cpuChoice === "rock") {
-                console.log("Its a Tie! Try Again!");
-            }
-            if (cpuChoice === "paper") {
-                console.log("Paper beats Rock, computer wins!");
-            }
-            if (cpuChoice === "scissors") {
-                console.log("Rock beats Scissors, you win!");
-            }
-            break;
-
-        case playerChoice === "scissors":
-            if (cpuChoice === "rock") {
-                console.log("Rock beats Scissors, computer wins!");
-            }
-            if (cpuChoice === "paper") {
-                console.log("Scissors beats Paper, you win!");
-            }
-            if (cpuChoice === "scissors") {
-                console.log("Its a Tie! Try Again!");
-            }
-
-            break;
-
-        case playerChoice === "paper":
-            if (cpuChoice === "rock") {
-                console.log("Paper beats Rock, you win!");
-            }
-            if (cpuChoice === "paper") {
-                console.log("Its a Tie! Try Again!");
-            }
-            if (cpuChoice === "scissors") {
-                console.log("Scissors beats paper, computer wins!");
-            }
-
-            break;
+        score.textContent = playerScore + " - " + computerScore;
+        results.textContent = "You win this round!";
 
     }
-*/
+
 }
 
 //plays 5 rounds of Rock, Paper, Scissors and decides a winner
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log(playerScore);
-        console.log(computerScore);
-    }
-    if (playerScore > computerScore) {
-        alert("You win! The score was " + playerScore + "-" + computerScore);
-    } else if (computerScore > playerScore) {
-        alert("Computer Wins! The score was " + playerScore + "-" + computerScore);
-    } else {
-        alert("Its a tie! The score was " + playerScore + "-" + computerScore);
+function playGame(choice) {
+    let playerChoice = choice.currentTarget.className;
+
+    playRound(playerChoice);
+    console.log(rounds);
+
+    // playRound(playerChoice);
+
+    if (rounds >= 5) {
+
+
+        if (playerScore > computerScore) {
+            results.textContent = "You win! The score was " + playerScore + "-" + computerScore;
+        } else if (computerScore > playerScore) {
+            results.textContent = "Computer Wins! The score was " + playerScore + "-" + computerScore;
+        } else {
+            results.textContent = "Its a tie! The score was " + playerScore + "-" + computerScore;
+        }
     }
 
 }
 
 //plays the game
 
-playGame();
 
 
 
